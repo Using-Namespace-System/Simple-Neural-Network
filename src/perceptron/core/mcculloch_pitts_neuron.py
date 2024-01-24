7bbe6cb7ed7331136eebdfd31f2cc8f1285fd34f
Brian Recktenwall-Calvet <RiverScriptTechServices@gmail.com>
2024-01-24 17:30:31 +0000

    individual units seem to be sharing an attribute that should be private

--- src/perceptron/core/mcculloch_pitts_neuron.py
+++ src/perceptron/core/mcculloch_pitts_neuron.py
@@ -6,36 +6,62 @@ class neuron:
     __matrix = [[]]
 
     class unit(object):
-        structure: 'neuron'
         layer = 0
         identifier = 0
         value = 0
-        parents = []
-        children = []
+        __parents: ['neuron.unit']
+        __children: ['neuron.unit']
+        __parent_mask: [int]
 
-        def __init__(self, layer: int, identifier: int, value: int, structure: 'neuron'):
+        def __init__(self, layer: int, identifier: int, value: int):
             self.value = value
             self.layer = layer
             self.identifier=identifier
-            self.structure = structure
         
-
         def set_parents(self, parents: ['neuron.unit']):
             for parent in parents:
-                self.structure.__matrix[parent.layer,parent.identifier].add_child(self)
-        def add_parent(self, parent: 'neuron.unit'):
-            self.structure.__matrix[parent.layer,parent.identifier].add_child(self)
-        def set_children(self, children: ['neuron.unit']):
-            for child in children:
-                self.structure.__matrix[child.layer,child.identifier].add_parent(self)
+                parent.add_child(self)
+                self.__parents.append(parent)
+        def add_parent(self, parent: 'neuron.unit', _mask: int):
+            print(_mask,((parent.layer,parent.identifier),(self.layer,self.identifier)))
+            print(parent)
+            print(self)
+            self.__parents.append(self,parent)
+            print(((self.layer,self.identifier),self.__parent_mask))
+            self.__parent_mask.append(_mask)
+            print(((self.layer,self.identifier),self.__parent_mask))
+            print(id(self.__parent_mask))
+        def set_children(self, children: ['neuron.unit'], _parent_masks: [int]):
+            if(len(children) == len(_parent_masks)):
+                print("Size: " + str((len(children),len(_parent_masks))))
+                for child, mask in zip(children, _parent_masks):
+                    #print((child.layer,child.identifier))
+                    child.add_parent(self,mask)
+                    self.children.append(child)
+            print("Size Mismach: " + str((len(children),len(_parent_masks))))
         def add_child(self, child: 'neuron.unit'):
-            self.structure.__matrix[child.layer,child.identifier].add_parent(self)
+            self.children.append(child)
+        
+        def input_signals(self):
+            #return np.array([parent.value * mask for parent,mask in zip(self.parents, self.parent_mask)])
+            return self.parents
         
     def fit(self,sensory_units:int = 64, association_units:int = 82, response_units:int = 2 ):
-        self.__matrix = [[neuron.unit(0,i,0,self) for i in range(0,sensory_units)],
-                    [neuron.unit(1,i,0,self) for i in range(0,association_units)],
-                    [neuron.unit(2,i,0,self) for i in range(0,response_units)]]
-        #for each sensory_units add up to 8 random children from the pool of association_units
+        self.__matrix = [[neuron.unit(0,i,0) for i in range(0,sensory_units)],
+                    [neuron.unit(1,i,0) for i in range(0,association_units)],
+                    [neuron.unit(2,i,0) for i in range(0,response_units)]]
+        
+        #for each sensory_units add up to 8 random children (half negative half positive) from the pool of association_units
+        mask = np.full(8, 1)
+        mask[:4] = -1
+        for s_unit in self.__matrix[0]:
+            np.random.shuffle(mask)
+            children = np.random.choice(association_units, 8,replace=False)
+            #print((s_unit.layer,s_unit.identifier))
+            #print(children)
+            #print(mask)
+            #print([(child.layer,child.identifier) for child in np.array(self.__matrix[1])[children]])
+            s_unit.set_children(np.array(self.__matrix[1])[children],mask)
 
     def sensory_units(self):
         #hard coded size
@@ -47,3 +73,6 @@ class neuron:
         else:
             print(len(self.__matrix[0]))
             print(len(vals))
+
+    def association_units(self):
+        return self.__matrix[1]
