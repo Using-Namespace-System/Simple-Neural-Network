8efb7d873f4f6dc4fbf09ca89575691e5076feb0
Brian Recktenwall-Calvet <RiverScriptTechServices@gmail.com>
2024-01-24 19:52:24 +0000

    namespace variables were not initiated in init.

--- src/perceptron/core/mcculloch_pitts_neuron.py
+++ src/perceptron/core/mcculloch_pitts_neuron.py
@@ -17,34 +17,26 @@ class neuron:
             self.value = value
             self.layer = layer
             self.identifier=identifier
+            self.__parents = []
+            self.__children = []
+            self.__parent_mask = []
         
         def set_parents(self, parents: ['neuron.unit']):
             for parent in parents:
                 parent.add_child(self)
                 self.__parents.append(parent)
         def add_parent(self, parent: 'neuron.unit', _mask: int):
-            print(_mask,((parent.layer,parent.identifier),(self.layer,self.identifier)))
-            print(parent)
-            print(self)
-            self.__parents.append(self,parent)
-            print(((self.layer,self.identifier),self.__parent_mask))
+            self.__parents.append(parent)
             self.__parent_mask.append(_mask)
-            print(((self.layer,self.identifier),self.__parent_mask))
-            print(id(self.__parent_mask))
         def set_children(self, children: ['neuron.unit'], _parent_masks: [int]):
-            if(len(children) == len(_parent_masks)):
-                print("Size: " + str((len(children),len(_parent_masks))))
-                for child, mask in zip(children, _parent_masks):
-                    #print((child.layer,child.identifier))
-                    child.add_parent(self,mask)
-                    self.children.append(child)
-            print("Size Mismach: " + str((len(children),len(_parent_masks))))
+            for child, mask in zip(children, _parent_masks):
+                child.add_parent(self,mask)
+                self.__children.append(child)
         def add_child(self, child: 'neuron.unit'):
             self.children.append(child)
         
         def input_signals(self):
-            #return np.array([parent.value * mask for parent,mask in zip(self.parents, self.parent_mask)])
-            return self.parents
+            return np.array([parent.value * mask for parent,mask in zip(self.__parents, self.__parent_mask)])
         
     def fit(self,sensory_units:int = 64, association_units:int = 82, response_units:int = 2 ):
         self.__matrix = [[neuron.unit(0,i,0) for i in range(0,sensory_units)],
@@ -57,10 +49,6 @@ class neuron:
         for s_unit in self.__matrix[0]:
             np.random.shuffle(mask)
             children = np.random.choice(association_units, 8,replace=False)
-            #print((s_unit.layer,s_unit.identifier))
-            #print(children)
-            #print(mask)
-            #print([(child.layer,child.identifier) for child in np.array(self.__matrix[1])[children]])
             s_unit.set_children(np.array(self.__matrix[1])[children],mask)
 
     def sensory_units(self):
@@ -71,8 +59,7 @@ class neuron:
         if len(self.__matrix[0]) == len(vals):
             for s_unit, val in zip(self.__matrix[0], vals): s_unit.value = val
         else:
-            print(len(self.__matrix[0]))
-            print(len(vals))
+            print("Size Mismatch: " + str((len(self.__matrix[0]), len(vals))))
 
     def association_units(self):
-        return self.__matrix[1]
+        return [a_unit.input_signals() for a_unit in self.__matrix[1]]
